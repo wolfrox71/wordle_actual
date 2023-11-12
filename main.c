5146234e682c4fa4fc01d4e38bf1043a45b4d8f5
wolfrox71 <wolfrox71@gmail.com>
2023-11-12 18:02:38 +0000

    Plan

---
+++ main.c
@@ -0,0 +1,24 @@
+
+/*
+What needs to be done:
+
+Generate word to guess
+Get input from the user to guess the word
+Make sure that the entered word is of the right length (5 chars)
+check the users word against the target word
+    Put green for correct place
+    Put Orange for incorrect place
+    Put grey for not in target
+
+Output word
+
+Check to see if the word has been guessed and/or if more guesses are avaiable
+
+If then guess again
+
+*/
+
+
+int main() {
+
+}
